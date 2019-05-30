aa440e93d0bdf4137372dacead1c13611a768d0e
Marco Vargas <mav89ls@gmail.com>
2019-05-29 23:42:22 -0700

    omg i did not commit the entire time because i was having such a hard time but i think MVP is donegit add .! so happygit add .

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from "./HeaderTitle";
+
+
+const HeaderContent = () => {
+    return ( 
+    <div className="header-content">
+    <HeaderTitle />
+    <span className="plain-text-top">Let 's learn React by building simple interfaces with components. Don'
+        t
+        try to overthink it, just keep it simple and have fun.Once you feel comfortable using componenets you are well on your way to mastering React! 
+        </span>
+    </div>
+    );
+};
+
+
+
+
+export default HeaderContent;
